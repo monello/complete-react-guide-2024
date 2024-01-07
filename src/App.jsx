1e9ecb996be7e0a1b22e3a4cf3efc1c12aafc0ae
Morne <louw.morne@gmail.com>
2024-01-07 08:08:02 +0000

    feat: add prop values from external data source

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -40,9 +40,24 @@ function App() {
                     <h2>Time to get started!</h2>
                     <ul>
                         <CoreConcepts
-                            title="Components"
-                            description="The core UI building blocks"
-                            imgage={componentsImg}
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            imgage={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcepts
+                            title={CORE_CONCEPTS[1].title}
+                            description={CORE_CONCEPTS[1].description}
+                            imgage={CORE_CONCEPTS[1].image}
+                        />
+                        <CoreConcepts
+                            title={CORE_CONCEPTS[2].title}
+                            description={CORE_CONCEPTS[2].description}
+                            imgage={CORE_CONCEPTS[2].image}
+                        />
+                        <CoreConcepts
+                            title={CORE_CONCEPTS[3].title}
+                            description={CORE_CONCEPTS[3].description}
+                            imgage={CORE_CONCEPTS[3].image}
                         />
                     </ul>
                 </section>
