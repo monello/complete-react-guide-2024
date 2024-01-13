f711f85c519cc4c7fa5d54ec9ab11dc9af4eb730
Morne <louw.morne@gmail.com>
2024-01-13 19:46:14 +0000

    feat: working with mutiple JSX slots

--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import TabButton from '../TabButton/TabButton';
 import Section from '../Section/Section';
+import Tabs from '../Tabs/Tabs';
 import { EXAMPLES } from '../../data';
 
 export default function Examples() {
@@ -12,42 +13,47 @@ export default function Examples() {
 
     return (
         <Section title="Examples" id="examples">
-            <menu>
-                <TabButton
-                    onClick={() => handleSelectTopic('components')}
-                    isSelected={selectedTopic === 'components'}
-                >
-                    Components
-                </TabButton>
-                <TabButton
-                    onClick={() => handleSelectTopic('jsx')}
-                    isSelected={selectedTopic === 'jsx'}
-                >
-                    JSX
-                </TabButton>
-                <TabButton
-                    onClick={() => handleSelectTopic('props')}
-                    isSelected={selectedTopic === 'props'}
-                >
-                    Props
-                </TabButton>
-                <TabButton
-                    onClick={() => handleSelectTopic('state')}
-                    isSelected={selectedTopic === 'state'}
-                >
-                    State
-                </TabButton>
-            </menu>
-            {!selectedTopic && <p>Please select a topic</p>}
-            {selectedTopic && (
-                <div id="tab-content">
-                    <h3>{EXAMPLES[selectedTopic].title}</h3>
-                    <p>{EXAMPLES[selectedTopic].description}</p>
-                    <pre>
-                        <code>{EXAMPLES[selectedTopic].code}</code>
-                    </pre>
-                </div>
-            )}
+            <Tabs
+                buttons={
+                    <>
+                        <TabButton
+                            onClick={() => handleSelectTopic('components')}
+                            isSelected={selectedTopic === 'components'}
+                        >
+                            Components
+                        </TabButton>
+                        <TabButton
+                            onClick={() => handleSelectTopic('jsx')}
+                            isSelected={selectedTopic === 'jsx'}
+                        >
+                            JSX
+                        </TabButton>
+                        <TabButton
+                            onClick={() => handleSelectTopic('props')}
+                            isSelected={selectedTopic === 'props'}
+                        >
+                            Props
+                        </TabButton>
+                        <TabButton
+                            onClick={() => handleSelectTopic('state')}
+                            isSelected={selectedTopic === 'state'}
+                        >
+                            State
+                        </TabButton>
+                    </>
+                }
+            >
+                {!selectedTopic && <p>Please select a topic</p>}
+                {selectedTopic && (
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>
+                            <code>{EXAMPLES[selectedTopic].code}</code>
+                        </pre>
+                    </div>
+                )}
+            </Tabs>
         </Section>
     );
 }
--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TabButton = ({ children, isSelected, ...props }) => {
     return (
         <li>
-            <button className={isSelected && 'active'} {...props}>
+            <button className={isSelected ? 'active' : ''} {...props}>
                 {children}
             </button>
         </li>
