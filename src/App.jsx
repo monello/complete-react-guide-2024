90667e9f3273161780f1efa07c5fe780a05a243a
Morne <louw.morne@gmail.com>
2024-01-14 17:41:38 +0000

    feat:  add basic player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Player from './components/Player';
+
 function App() {
-    return <p>Coming soon...</p>;
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <Player name="Player 1" symbol="X" />
+                    <Player name="Player 2" symbol="O" />
+                </ol>
+                GAME BOARD WILL GO HERE
+            </div>
+            GAMES LOG WILL GO HERE
+        </main>
+    );
 }
 
 export default App;
