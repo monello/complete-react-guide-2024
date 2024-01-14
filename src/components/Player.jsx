d457f882f305b30d0cd956db6c1072422bdd9364
Morne <louw.morne@gmail.com>
2024-01-14 17:50:59 +0000

    feat: state-management with useState

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
+
 export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const toggleEditMode = () => {
+        setIsEditing((mode) => !mode);
+    };
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {!isEditing && <span className="player-name">{name}</span>}
+                {isEditing && <input value={name} />}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={toggleEditMode}>
+                {isEditing ? 'Save' : 'Edit'}
+            </button>
         </li>
     );
 }
