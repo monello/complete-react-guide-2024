40d461e0a8fda73f51f5405e48ac2d82518a2b9c
Morne <louw.morne@gmail.com>
2024-01-14 17:52:16 +0000

    feat: two-way-binding with useState

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,17 +1,24 @@
 import { useState } from 'react';
 
 export default function Player({ name, symbol }) {
+    const [nameValue, setNameValue] = useState(name);
     const [isEditing, setIsEditing] = useState(false);
 
     const toggleEditMode = () => {
         setIsEditing((mode) => !mode);
     };
 
+    const onNameChange = (event) => {
+        setNameValue(event.target.value);
+    };
+
     return (
         <li>
             <span className="player">
-                {!isEditing && <span className="player-name">{name}</span>}
-                {isEditing && <input value={name} />}
+                {!isEditing && <span className="player-name">{nameValue}</span>}
+                {isEditing && (
+                    <input value={nameValue} onChange={onNameChange} />
+                )}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={toggleEditMode}>
