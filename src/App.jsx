72d471aba9e631f15d5ad99c7246866a8276d9f9
Morne <louw.morne@gmail.com>
2024-01-15 06:01:00 +0000

    feat: add initial game-board structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Player from './components/Player';
+import GameBoard from './components/GameBoard';
 
 function App() {
     return (
         <main>
             <div id="game-container">
                 <ol id="players">
-                    <Player name="Player 1" symbol="X" />
-                    <Player name="Player 2" symbol="O" />
+                    <Player initialName="Player 2" symbol="O" />
+                    <Player initialName="Player 1" symbol="X" />
                 </ol>
-                GAME BOARD WILL GO HERE
+                <GameBoard />
             </div>
             GAMES LOG WILL GO HERE
         </main>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,34 @@
 import { useState } from 'react';
 
-export default function Player({ name, symbol }) {
-    const [nameValue, setNameValue] = useState(name);
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
-    const toggleEditMode = () => {
-        setIsEditing((mode) => !mode);
+    const handleEditClick = () => {
+        setIsEditing((currEditMode) => !currEditMode);
     };
 
-    const onNameChange = (event) => {
-        setNameValue(event.target.value);
+    const handleNameChange = (event) => {
+        setPlayerName(event.target.value);
     };
 
     return (
         <li>
             <span className="player">
-                {!isEditing && <span className="player-name">{nameValue}</span>}
+                {!isEditing && (
+                    <span className="player-name">{playerName}</span>
+                )}
                 {isEditing && (
-                    <input value={nameValue} onChange={onNameChange} />
+                    <input
+                        type="text"
+                        required
+                        value={playerName}
+                        onChange={handleNameChange}
+                    />
                 )}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={toggleEditMode}>
+            <button onClick={handleEditClick}>
                 {isEditing ? 'Save' : 'Edit'}
             </button>
         </li>
