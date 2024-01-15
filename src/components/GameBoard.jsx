f45ed26345617100545c017e0f8f1f08a57119d9
Morne <louw.morne@gmail.com>
2024-01-15 06:28:50 +0000

    feat: immutably update complex state

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 const initialGameBoard = [
     [null, null, null],
@@ -7,14 +7,33 @@ const initialGameBoard = [
 ];
 
 function GameBoard() {
+    const [gameBoard, setGameboard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameboard((currGameBoard) => {
+            // We need to use this functional form to update the state, because we are dependent on the current (guaranteed latest version)
+            //  of the state-managed value
+            // Make a deep-copy of the board before applying changes
+            const updatedBoard = [...currGameBoard.map((row) => [...row])];
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button
+                                    onClick={() =>
+                                        handleSelectSquare(rowIndex, colIndex)
+                                    }
+                                >
+                                    {playerSymbol}
+                                </button>
                             </li>
                         ))}
                     </ol>
