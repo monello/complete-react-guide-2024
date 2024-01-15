1f9de6915cf15d06a492a37cb3fea12ca158387a
Morne <louw.morne@gmail.com>
2024-01-15 06:14:04 +0000

    feat: add initial game-board structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import GameBoard from './components/GameBoard';
 import Player from './components/Player';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
                     <Player initialName="Player 1" symbol="X" />
                     <Player initialName="Player 2" symbol="O" />
                 </ol>
-                GAME BOARD WILL GO HERE
+                <GameBoard />
             </div>
             GAMES LOG WILL GO HERE
         </main>
