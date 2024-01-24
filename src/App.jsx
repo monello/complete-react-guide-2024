f92b42fa0b051bd2f6029774bcbd2a69b7c0dabd
Morne <louw.morne@gmail.com>
2024-01-24 17:49:40 +0000

    feat: reduce more state-management with derived state

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import GameBoard from './components/GameBoard';
 import Player from './components/Player';
 import Log from './components/Log';
 
+function getActicePlayer(gameTurns) {
+    let currentPlayer = 'X';
+    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+        currentPlayer = 'O';
+    }
+    return currentPlayer;
+}
+
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
-    const [activePlayer, setActivePlayer] = useState('X');
+
+    const activePlayer = getActicePlayer(gameTurns);
 
     const handleSelectSquare = (roIndex, colIndex) => {
-        setActivePlayer((currPlayer) => (currPlayer === 'X' ? 'O' : 'X'));
         setGameTurns((prevTurns) => {
-            let currentPlayer = 'X';
-            if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-                currentPlayer = 'O';
-            }
+            const currentPlayer = getActicePlayer(prevTurns);
+
             const updatedTurns = [
                 {
                     square: { row: roIndex, col: colIndex },
