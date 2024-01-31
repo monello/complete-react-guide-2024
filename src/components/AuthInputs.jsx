e0823f92bd8175e0decae39ed907ec025f3b7a9a
Morne <louw.morne@gmail.com>
2024-01-31 20:36:52 +0000

    feat: more dynamic styles using style-components

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -23,9 +23,9 @@ const StyledInput = styled.input`
     width: 100%;
     padding: 0.75rem 1rem;
     line-height: 1.5;
-    background-color: #d1d5db;
-    color: #374151;
-    border: 1px solid transparent;
+    background-color: ${({ invalid }) => (invalid ? '#fed2d2' : '#d1d5db')};
+    color: ${({ invalid }) => (invalid ? '#ef4444' : '#374151')};
+    border: 1px solid ${({ invalid }) => (invalid ? '#f73f3f' : 'transparent')};
     border-radius: 0.25rem;
     box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
@@ -57,7 +57,7 @@ export default function AuthInputs() {
                     <StyledLabel invalid={emailNotValid}>Email</StyledLabel>
                     <StyledInput
                         type="email"
-                        className={emailNotValid ? 'invalid' : undefined}
+                        invalid={emailNotValid}
                         onChange={(event) =>
                             handleInputChange('email', event.target.value)
                         }
@@ -69,7 +69,7 @@ export default function AuthInputs() {
                     </StyledLabel>
                     <StyledInput
                         type="password"
-                        className={passwordNotValid ? 'invalid' : undefined}
+                        invalid={passwordNotValid}
                         onChange={(event) =>
                             handleInputChange('password', event.target.value)
                         }
